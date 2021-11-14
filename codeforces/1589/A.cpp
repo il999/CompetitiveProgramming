ffa8ad8ceb421870c3a3c4177075826ccf1668cd
Isaac Lee <isaacanthonylee@gmail.com>
2021-11-14 07:15:00 +0000

    Add solution for problem `A - Mathematical Addition` Link: https://codeforces.com/contest/1589/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1589/submission/135374298

---
+++ codeforces/1589/A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    long long a,b;
+    cin>>a>>b;
+    cout<<-(a*a)<<" "<<b*b<<endl;
+
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
