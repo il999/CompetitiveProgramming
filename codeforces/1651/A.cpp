8ed56eb9da66f532e322b0d1a426e7c1b2461df5
Isaac Lee <isaacanthonylee@gmail.com>
2022-03-10 14:38:00 +0000

    Add solution for problem `A - Playoff` Link: https://codeforces.com/contest/1651/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1651/submission/149094690

---
+++ codeforces/1651/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+void solve()
+{
+    int n;
+    cin>>n;
+    int winner=(1<<(n))-1;
+    cout<<winner<<"\n";
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+}
