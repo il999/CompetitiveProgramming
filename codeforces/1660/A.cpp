3123387c6b5e1ac4fe60ab3a016a7e98e91bde3f
Isaac Lee <isaacanthonylee@gmail.com>
2022-04-02 09:31:00 +0100

    Add solution for problem `A - Vasya and Coins` Link: https://codeforces.com/contest/1660/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1660/submission/152467719

---
+++ codeforces/1660/A.cpp
@@ -0,0 +1,22 @@
+//coding template
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a==0)
+    {
+        cout<<1<<"\n";
+        return;
+    }
+    cout<<b*2+a+1<<"\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+}
