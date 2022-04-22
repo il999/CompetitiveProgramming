0b89b4d9fb177baa1e8b978cd1573bf75525aab6
Isaac Lee <isaacanthonylee@gmail.com>
2022-04-22 21:21:00 +0100

    Add solution for problem `B - Consecutive Points Segment` Link: https://codeforces.com/contest/1671/problem/B Tags: brute force, math, sortings, *1000 Ref: https://codeforces.com/contest/1671/submission/154595323

--- codeforces/1671/B.cpp
+++ codeforces/1671/B.cpp
@@ -13,40 +13,45 @@ void solve()
     int p=0;
     bool three=false;
     int two=0;
+
+    bool great=false;
     for(int i=1;i<n;i++)
     {
         if(l[i]-l[i-1]>1)
         {
+           // cout<<"DTGSG";
             if(l[i]-l[i-1]>3)
             {
                 cout<<"NO"<<"\n";
                 return;
             }
-            if(l[i]-l[i-1]==2)
+            if(great)
             {
-
-                if(two>1)
-                {
-                    cout<<"NO"<<"\n";
-                    return;
-                }
+                cout<<"NO"<<"\n";
+                return;
+            }
+            if(l[i]-l[i-1]!=2)
+            {
+                great=true;
+            }else
+            {
                 two++;
 
             }
-            if(l[i]-l[i-1]==3)
-            {
-                if(three)
-                {
-                    cout<<"NO"<<"\n";
-                    return;
-                }
-                three=true;
-            }
-            if(three&&two>0)
+            if(two>0&&great)
             {
                 cout<<"NO \n";
                 return;
             }
+            if(two>2)
+            {
+                cout<<"NO \n";
+                return;
+            }
+
+
+
+
         }
 
     }
@@ -55,7 +60,7 @@ void solve()
 }
 int main()
 {
-    ios_base::sync_with_stdio(0); cin.tie(0);
+    //ios_base::sync_with_stdio(0); cin.tie(0);
     int t;
     cin>>t;
     while(t--)
