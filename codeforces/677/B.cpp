89961401192d24fc23f5e401d95302bf2ca87d6d
Isaac Lee <isaacanthonylee@gmail.com>
2022-07-18 11:35:00 +0100

    Add solution for problem `B - Vanya and Food Processor` Link: https://codeforces.com/contest/677/problem/B Tags: implementation, math, *1400 Ref: https://codeforces.com/contest/677/submission/164709066

---
+++ codeforces/677/B.cpp
@@ -0,0 +1,64 @@
+//coding template
+/*
+⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆
+⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿
+⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀
+⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀
+⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉
+*/
+#include <bits/stdc++.h>
+using ll =long long;
+using namespace std;
+void solve()
+{
+    int n,k,h;
+    cin>>n>>h>>k;
+    int a[n];
+    long long total=0;
+    long long time=0;
+    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<n;i++)
+    {
+        if(total+a[i]>h)
+        {
+            time+=(total-(h-a[i]))/k;
+            if((total-(h-a[i]))%k)
+            {
+                time++;
+                //total=
+                total-=(((total-(h-a[i]))/k)+1)*k;
+                long long zero=0;
+                total=max( zero,total);
+            }else
+            {
+                total=(h-a[i]);
+            }
+
+
+
+        }
+            total+=a[i];
+
+
+
+    }
+    time+=total/k;
+    if(total%k) time++;
+    cout<<time<<"\n";
+}
+int main()
+{
+    //ios_base::sync_with_stdio(0); cin.tie(0);
+
+        solve();
+}
