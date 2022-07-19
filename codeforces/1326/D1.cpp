455056d8c8b164c76a0df5d62178bdb1073beac0
Isaac Lee <isaacanthonylee@gmail.com>
2022-07-19 19:49:00 +0100

    Add solution for problem `D1 - Prefix-Suffix Palindrome (Easy version)` Link: https://codeforces.com/contest/1326/problem/D1 Tags: hashing, string suffix structures, strings, *1500 Ref: https://codeforces.com/contest/1326/submission/164934161

--- codeforces/1326/D1.cpp
+++ codeforces/1326/D1.cpp
@@ -19,95 +19,55 @@
 #include <bits/stdc++.h>
 using ll =long long;
 using namespace std;
-const int MOD=1e9+7;
-int string_hashing(string s)
+string LongestPalindromicPrefix(string str)
 {
 
-    int n=s.size();
-    //hash
-    ll h[n+1];
-    //hash backwards;
-    ll h2[n+1];
-    h[n]=0;
-    h[0]=0;
-    ll p=31;
-    ll temp=1;
-    ll p1[n];
+    // Create temporary string
+    string temp = str + '?';
 
-    for(int i=0;i<n;i++)
-    {
-        p1[i]=temp%MOD;
-        h[i+1]=(h[i]+temp*(s[i]-'a'+1)) %MOD;
-        temp=(temp*p)%MOD;
+    // Reverse the string str
+    reverse(str.begin(), str.end());
 
+    // Append string str to temp
+    temp += str;
 
-    }
-    temp=1;
-    for(int i=n-1;i>=0;i--)
-    {
+    // Find the length of string temp
+    int n = temp.length();
 
-        h2[i]=(h2[i+1]+temp*(s[i]-'a'+1))%MOD;
-        temp=(temp*p)%MOD;
-    }
-    ll hashval1=0;
-    ll hashval2=0;
-    ll ans=0;
-    for(int i=0;i<n/2;i++)
-    {
-       // cout<<h[i+2]<<"\n";
-        //cout<<p1[10]<<"\n";
-        hashval1=h[i+1];
-        if((i+1)*2<n)
-        {
-            hashval2=(h2[(i+2)]-h2[(i+1)*2+1])%MOD;
-            if(hashval2<0)
-            {
-                hashval2=MOD+hashval2;
-            }
-            if((n-(i+2)<i+1))
-            {
-                hashval2=(hashval2*(p1[i+1-(n-(i+2))]))%MOD;
-            }else
-            {
-                hashval1=(hashval1*(p1[n-(i+2)-(i+1)]))%MOD;
-            }
-           // cout<<hashval1<<" "<<hashval2<<"\n";
+    // lps[] array for string temp
+    int lps[n];
 
-            if(hashval1==hashval2)
-            {
-                ans=(i+1)*2;
-             //  cout<<"DFS";
-            }
+    // Initialise every value with zero
+    fill(lps, lps + n, 0);
 
-        }
-        hashval1=h[i+1];
-        if((i+1)*2<=n)
-        {
-            hashval2=(h2[(i+1)]-h2[(i+1)*2])%MOD;
-            if(hashval2<0)
-            {
-                hashval2=MOD+hashval2;
-            }
-            if((n-(i+1))<i+1)
-            {
-                hashval2=(hashval2*(p1[(i+1)-(n-(i+1))]))%MOD;
-            }else
-            {
-                hashval1=(hashval1*(p1[(n-(i+1))-(i+1)]))%MOD;
-            }
-           // cout<<(hashval1*p1[10])%MOD<<" "<<hashval2<<"\n";
-            if(hashval1==hashval2)
-            {
-                ans=max(1ll*(i+1)*2-1,ans);
-              //  cout<<"DS";
-            }
+    // Iterate the string temp
+    for (int i = 1; i < n; i++) {
 
+        // Length of longest prefix
+        // till less than i
+        int len = lps[i - 1];
+
+        // Calculate length for i+1
+        while (len > 0
+               && temp[len] != temp[i]) {
+            len = lps[len - 1];
         }
 
+        // If character at current index
+        // len are same then increment
+        // length by 1
+        if (temp[i] == temp[len]) {
+            len++;
+        }
+
+        // Update the length at current
+        // index to len
+        lps[i] = len;
     }
-    return ans;
 
-
+    // Print the palindromic string of
+    // max_len
+    return temp.substr(0, lps[n - 1]);
 }
 void solve()
 {
@@ -132,34 +92,20 @@ void solve()
     {
         s2+=s[i];
     }
-     string middle="";
-    if(s2.size()!=0)
-    {
-        int  prefix= string_hashing(s2);
-        string backwardss2="";
-        for(int i=s2.size()-1;i>=0;i--)
-        {
-            backwardss2+=s2[i];
-        }
-        int  suffix= string_hashing(backwardss2);
-         if(prefix>suffix)
-            {
-                for(int i=0;i<=prefix;i++)
-                {
-                    middle+=s2[i];
-                }
-        }else
-            {
-                for(int i=0;i<=suffix;i++)
-                {
-                    middle+=backwardss2[i];
-                }
-            }
-    }
+
    // cout<<s2<<"\n";
-
-
-
+    string  prefix= LongestPalindromicPrefix(s2);
+    string backwardss2="";
+    for(int i=s2.size()-1;i>=0;i--)
+    {
+        backwardss2+=s2[i];
+    }
+    string  suffix= LongestPalindromicPrefix(backwardss2);
+    string middle=prefix;
+    if(suffix.size()>prefix.size())
+    {
+        middle=suffix;
+    }
     for(int i=0;i<in;i++)
     {
         cout<<s[i];
