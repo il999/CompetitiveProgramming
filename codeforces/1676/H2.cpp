492d8ac16dc289d76e1083e5bcd6287c1dc5204d
Isaac Lee <isaacanthonylee@gmail.com>
2022-07-21 09:01:00 +0100

    Add solution for problem `H2 - Maximum Crossings (Hard Version)` Link: https://codeforces.com/contest/1676/problem/H2 Tags: data structures, divide and conquer, sortings, *1500 Ref: https://codeforces.com/contest/1676/submission/165092236

---
+++ codeforces/1676/H2.cpp
@@ -0,0 +1,62 @@
+//coding template
+/*
+⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆
+⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿
+⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀
+⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀
+⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉
+*/
+#include <bits/stdc++.h>
+using ll =long long;
+using namespace std;
+struct BIT {
+    int n,rtn;
+    vector<int> bit;
+    BIT(int n) : n(n), bit(n+1,0) {}
+    void update(int x, int d) { for (int i=x;i<=n;i+=i&-i) bit[i]+=d; }
+    int query(int x) {
+        rtn=0;
+        for (int i=x;i;i-=i&-i) rtn+=bit[i];
+        return rtn;
+    }
+    int query(int x, int y) { return query(y) - query(x-1); }
+};
+void solve()
+{
+    int n;
+    cin>>n;
+    int l[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>l[i];
+
+    }
+    BIT bit(n);
+    ll ans=0;
+    for(int i=n-1;i>=0;i--)
+    {
+        ans+=bit.query(l[i]);
+        bit.update(l[i],1);
+        //cout<<ans<<"\n";
+
+    }
+    cout<<ans<<"\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+}
