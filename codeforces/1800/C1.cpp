eb2d19738fa866aae4067389b571e32d121f5c8d
Isaac Lee <isaacanthonylee@gmail.com>
2023-03-06 18:33:00 +0000

    Add solution for problem `C1 - Powering the Hero (easy version)` Link: https://codeforces.com/contest/1800/problem/C1 Tags: data structures, greedy, *1000 Ref: https://codeforces.com/contest/1800/submission/196299347

---
+++ codeforces/1800/C1.cpp
@@ -0,0 +1,73 @@
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <array>
+using namespace std;
+using ll=long long;
+const int MOD=998244353;
+int add(int x, int y)
+{
+    return ((x + y) % MOD + MOD) % MOD;
+}
+
+int mul(int x, int y)
+{
+    return x * 1ll * y % MOD;   
+}
+
+int binpow(int x, int y)
+{
+    int z = 1;
+    while(y)
+    {
+        if(y % 2 == 1) z = mul(z, x);
+        x = mul(x, x);
+        y /= 2;
+    }
+    return z;
+}
+
+int inv(int x)
+{
+    return binpow(x, MOD - 2);    
+}
+
+int divide(int x, int y)
+{
+    return mul(x, inv(y));
+}
+void solve(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    ll ans=0;
+    priority_queue<int> pq;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]!=0){
+            pq.push(a[i]);
+        }else if(!pq.empty())
+        {
+            ans+=pq.top();
+            pq.pop();
+        }
+    }
+    cout<<ans<<"\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+}
