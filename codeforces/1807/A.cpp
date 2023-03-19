78ac4fe0b44924daaa23085096244636e4b36538
Isaac Lee <isaacanthonylee@gmail.com>
2023-03-19 14:57:00 +0000

    Add solution for problem `A - Plus or Minus` Link: https://codeforces.com/contest/1807/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1807/submission/198115325

---
+++ codeforces/1807/A.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <array>
+using namespace std;
+using ll=long long;
+const int MOD=998244353;
+int add(int x, int y)
+{
+    return ((x + y) % MOD + MOD) % MOD;
+}
+
+int mul(int x, int y)
+{
+    return x * 1ll * y % MOD;   
+}
+
+int binpow(int x, int y)
+{
+    int z = 1;
+    while(y)
+    {
+        if(y % 2 == 1) z = mul(z, x);
+        x = mul(x, x);
+        y /= 2;
+    }
+    return z;
+}
+
+int inv(int x)
+{
+    return binpow(x, MOD - 2);    
+}
+
+int divide(int x, int y)
+{
+    return mul(x, inv(y));
+}
+void solve(){
+   
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(c<a){
+        cout<<"-\n";
+    }
+    if(c>a){
+        cout<<"+\n";
+    }
+    
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+}
