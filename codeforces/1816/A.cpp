ec33b58d5cb74b004f85dcc8378b1f6993a9fef3
Isaac Lee <isaacanthonylee@gmail.com>
2023-04-09 15:53:00 +0100

    Add solution for problem `A - Ian Visits Mary` Link: https://codeforces.com/contest/1816/problem/A Tags: constructive algorithms, geometry, number theory, *800 Ref: https://codeforces.com/contest/1816/submission/201485509

---
+++ codeforces/1816/A.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <array>
+using namespace std;
+using ll=long long;
+const int MOD=998244353;
+int add(int x, int y)
+{
+    return ((x + y) % MOD + MOD) % MOD;
+}
+
+int mul(int x, int y)
+{
+    return x * 1ll * y % MOD;   
+}
+
+int binpow(int x, int y)
+{
+    int z = 1;
+    while(y)
+    {
+        if(y % 2 == 1) z = mul(z, x);
+        x = mul(x, x);
+        y /= 2;
+    }
+    return z;
+}
+
+int inv(int x)
+{
+    return binpow(x, MOD - 2);    
+}
+
+int divide(int x, int y)
+{
+    return mul(x, inv(y));
+}
+void solve(){
+   
+    int a,b;
+    cin>>a>>b;
+
+        cout<<"2\n";
+        cout<<"1 "<<b-1<<"\n";
+        cout<<a<<" "<<b<<"\n";
+        return;
+    
+    
+    
+    
+    
+
+}
+int main()
+{
+   // ios_base::sync_with_stdio(0); cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+}
