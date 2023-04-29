7feb782759369b68304dc74a5e638194da1eb31c
Isaac Lee <isaacanthonylee@gmail.com>
2023-04-29 23:37:00 +0100

    Add solution for problem `C - Almost Increasing Subsequence` Link: https://codeforces.com/contest/1818/problem/C Tags: binary search, dp, greedy, *1500 Ref: https://codeforces.com/contest/1818/submission/203984837

--- codeforces/1818/C.cpp
+++ codeforces/1818/C.cpp
@@ -21,17 +21,36 @@ int main()
     cin>>n>>q;
     int a[n+1];
     for(int i=0;i<n;i++) cin>>a[i];
-
+    
+    vector<int> start(n+1);
+    vector<int> end(n+1);
     vector<int> totpre(n+1);
-    for(int i=1;i<n-1;i++){
-        if(a[i-1]>=a[i]&&a[i]>=a[i+1]){
-            totpre[i+1]++;
+    vector<bool> add(n+1);
+    vector<bool> started(n+2);
+    vector<bool> ended(n+1);
+    for(int i=0;i<n;i++){
+        if(i<n-2&&a[i]>=a[i+1]&&a[i+1]>=a[i+2]){
+            add[i]=1;
+            add[i+1]=1;
+            add[i+2]=1;
         }
-        totpre[i+1]+=totpre[i];
-        
-        
+        if(add[i]&&(i==n-1||a[i]<a[i+1])){
+            end[i+1]++;
+            ended[i+1]=1;
+        }
+        if(add[i]&&(i==0||a[i]>a[i-1])){
+            start[i+1]++;
+            started[i+1]=1;
+        }
+        if(add[i])totpre[i+1]++;
     }
-    totpre[n]+=totpre[n-1];
+    for(int i=1;i<=n;i++){
+        
+        start[i]+=start[i-1];
+        end[i]+=end[i-1];
+        totpre[i]+=totpre[i-1];
+        //cout<<start[i]<<" ";
+    }
    // cout<<" aa\n";
 
     while(q--){
@@ -41,8 +60,15 @@ int main()
             cout<<"1\n";
             continue;
         }
+        int reduce=0;
+        if(!started[r]&&!ended[r]&&add[r-1]){
+            reduce++;
+        }
+         if(!ended[l]&&!started[l]&&add[l-1]){
+            reduce++;
+        }
         //cout<<totpre[r]-totpre[l-1]<<" "<<start[r]-start[l-1]<<" "<<end[r]-end[l-1]<<"\n";
-        cout<<r-l+1-(totpre[r-1]-totpre[l])<<"\n";
+        cout<<r-l+1-(totpre[r]-totpre[l-1]-(start[r]-start[l-1])-(end[r]-end[l-1])-reduce)<<"\n";
     }
    
   
