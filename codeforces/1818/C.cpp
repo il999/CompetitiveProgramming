8848aee91525178385cc36d28548790ded1a1efa
Isaac Lee <isaacanthonylee@gmail.com>
2023-04-30 09:13:00 +0100

    Add solution for problem `C - Almost Increasing Subsequence` Link: https://codeforces.com/contest/1818/problem/C Tags: binary search, dp, greedy, *1500 Ref: https://codeforces.com/contest/1818/submission/204015334

---
+++ codeforces/1818/C.cpp
@@ -0,0 +1,55 @@
+#include <iostream> 
+#include <map>
+#include <set>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <array>
+using ll= long long;
+using namespace std;
+
+const int INF=1e9+7;
+int n;
+//vector<int> a;
+
+ 
+int main()
+{
+   // cin.tie(0);
+   cin.tie(0); ios_base::sync_with_stdio(0);
+    int q;
+    cin>>n>>q;
+    int a[n+1];
+    for(int i=0;i<n;i++) cin>>a[i];
+
+    vector<int> totpre(n+1);
+    for(int i=1;i<n-1;i++){
+        if(a[i-1]>=a[i]&&a[i]>=a[i+1]){
+            totpre[i+1]++;
+        }
+        totpre[i+1]+=totpre[i];
+        
+        
+    }
+    totpre[n]+=totpre[n-1];
+   // cout<<" aa\n";
+
+    while(q--){
+        int l,r;
+        cin>>l>>r;
+        if(l==r){
+            cout<<"1\n";
+            continue;
+        }
+        //cout<<totpre[r]-totpre[l-1]<<" "<<start[r]-start[l-1]<<" "<<end[r]-end[l-1]<<"\n";
+        cout<<r-l+1-(totpre[r-1]-totpre[l])<<"\n";
+    }
+   
+  
+
+    //update(0,1,n,1,1,0);
+    
+    
+
+
+}
