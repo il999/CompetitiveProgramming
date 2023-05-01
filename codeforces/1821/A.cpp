e28bfe0de973949ca97f93d6b5c6367079710d68
Isaac Lee <isaacanthonylee@gmail.com>
2023-05-01 19:53:00 +0100

    Add solution for problem `A - Matching` Link: https://codeforces.com/contest/1821/problem/A Tags: combinatorics, math, *800 Ref: https://codeforces.com/contest/1821/submission/204169154

---
+++ codeforces/1821/A.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <array>
+using namespace std;
+using ll=long long;
+const int MOD=998244353;
+int add(int x, int y)
+{
+    return ((x + y) % MOD + MOD) % MOD;
+}
+
+int mul(int x, int y)
+{
+    return x * 1ll * y % MOD;   
+}
+
+int binpow(int x, int y)
+{
+    int z = 1;
+    while(y)
+    {
+        if(y % 2 == 1) z = mul(z, x);
+        x = mul(x, x);
+        y /= 2;
+    }
+    return z;
+}
+
+int inv(int x)
+{
+    return binpow(x, MOD - 2);    
+}
+
+int divide(int x, int y)
+{
+    return mul(x, inv(y));
+}
+
+void solve(){
+    string s;
+    cin>>s;
+    int ans=1;
+    int que=0;
+    for(int i=1;i<s.size();i++){
+        if(s[i]=='?'){
+            ans*=10;
+        }
+    }
+    if(s[0]=='?')ans*=9;
+    if(s[0]=='0'){
+        cout<<"0\n";
+        return;
+    }
+    cout<<ans<<"\n";
+
+   
+
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+}
