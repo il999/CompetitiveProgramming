857a783cb20d11a1f6b927c55483925b6c056146
Isaac Lee <isaacanthonylee@gmail.com>
2023-05-08 16:31:00 +0100

    Add solution for problem `A - LuoTianyi and the Palindrome String` Link: https://codeforces.com/contest/1825/problem/A Tags: greedy, strings, *800 Ref: https://codeforces.com/contest/1825/submission/205136691

---
+++ codeforces/1825/A.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <array>
+using namespace std;
+using ll=long long;
+const int MOD=998244353;
+int add(int x, int y)
+{
+    return ((x + y) % MOD + MOD) % MOD;
+}
+
+int mul(int x, int y)
+{
+    return x * 1ll * y % MOD;   
+}
+
+int binpow(int x, int y)
+{
+    int z = 1;
+    while(y)
+    {
+        if(y % 2 == 1) z = mul(z, x);
+        x = mul(x, x);
+        y /= 2;
+    }
+    return z;
+}
+
+int inv(int x)
+{
+    return binpow(x, MOD - 2);    
+}
+
+int divide(int x, int y)
+{
+    return mul(x, inv(y));
+}
+
+void solve(){
+    int n;
+    string s;
+    cin>>s;
+    n=s.size();
+    bool palin=1;
+    vector<int> alpha(26);
+    for(int i=0;i<n;i++){
+        alpha[s[i]-'a']++;
+    }
+    
+    
+        sort(alpha.rbegin(),alpha.rend());
+        
+    if(alpha[0]<n){
+        cout<<n-1<<"\n";
+    }else{
+        cout<<"-1\n";
+    }
+
+
+}
+int main()
+{
+   // ios_base::sync_with_stdio(0); cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+}
