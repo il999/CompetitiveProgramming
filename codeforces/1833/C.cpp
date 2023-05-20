2131159aa8fba0331e7705f9f8322b6425a36a2b
Isaac Lee <isaacanthonylee@gmail.com>
2023-05-20 13:26:00 +0100

    Add solution for problem `C - Vlad Building Beautiful Array` Link: https://codeforces.com/contest/1833/problem/C Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1833/submission/206639489

---
+++ codeforces/1833/C.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <array>
+#include <stack>
+using namespace std;
+using ll=long long;
+const int MOD=998244353;
+int add(int x, int y)
+{
+    return ((x + y) % MOD + MOD) % MOD;
+}
+
+int mul(int x, int y)
+{
+    return x * 1ll * y % MOD;   
+}
+
+int binpow(int x, int y)
+{
+    int z = 1;
+    while(y)
+    {
+        if(y % 2 == 1) z = mul(z, x);
+        x = mul(x, x);
+        y /= 2;
+    }
+    return z;
+}
+
+int inv(int x)
+{
+    return binpow(x, MOD - 2);    
+}
+
+int divide(int x, int y)
+{
+    return mul(x, inv(y));
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    int a[n];
+    int mi=1e9+8;
+    for(int i=0;i<n;i++) cin>>a[i];
+    sort(a,a+n);
+    for(int i=0;i<n;i++){
+        if(a[i]%2==1)mi=min(a[i],mi);
+    }
+    if(mi==1e9+8){
+        cout<<"YES\n";
+        return;
+    }
+    bool pos=1;
+    for(int i=0;i<n;i++){
+        if(a[i]%2==0&&a[i]-mi<=0){
+            pos=0;
+        }
+    }
+    if(pos){
+        cout<<"YES\n";
+    }else{
+        cout<<"NO\n";
+    }
+
+    
+    
+
+
+}
+int main()
+{
+   // ios_base::sync_with_stdio(0); cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+}
