602c445486b4bce328c15cbc8c6d310b567a6e89
Isaac Lee <isaacanthonylee@gmail.com>
2023-06-04 15:54:00 +0100

    Add solution for problem `A - Blackboard List` Link: https://codeforces.com/contest/1838/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1838/submission/208442321

---
+++ codeforces/1838/A.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <array>
+#include <stack>
+using namespace std;
+using ll=long long;
+const int MOD=1e9+7;
+int add(int x, int y)
+{
+    return ((x + y) % MOD + MOD) % MOD;
+}
+
+int mul(int x, int y)
+{
+    return x * 1ll * y % MOD;   
+}
+
+int binpow(int x, int y)
+{
+    int z = 1;
+    while(y)
+    {
+        if(y % 2 == 1) z = mul(z, x);
+        x = mul(x, x);
+        y /= 2;
+    }
+    return z;
+}
+
+int inv(int x)
+{
+    return binpow(x, MOD - 2);    
+}
+
+int divide(int x, int y)
+{
+    return mul(x, inv(y));
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    ll a[n];
+    int neg=0;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        if(a[i]<0) neg++;
+    }
+    
+    sort(a,a+n);
+    if(neg>0){
+        cout<<a[0]<<"\n";
+        return;
+    }
+    cout<<a[n-1]<<"\n";
+
+
+
+}
+int main()
+{
+   // ios_base::sync_with_stdio(0); cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+}
