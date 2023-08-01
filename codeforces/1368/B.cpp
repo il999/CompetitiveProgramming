1982c91e4ccd1c18f94179fbd75d13bc0c98158e
Isaac Lee <isaacanthonylee@gmail.com>
2023-08-01 20:49:00 +0100

    Add solution for problem `B - Codeforces Subsequences` Link: https://codeforces.com/contest/1368/problem/B Tags: brute force, constructive algorithms, greedy, math, strings, *1500 Ref: https://codeforces.com/contest/1368/submission/216762181

--- codeforces/1368/B.cpp
+++ codeforces/1368/B.cpp
@@ -1,23 +1,26 @@
-#include <iostream>
+// Example program
+#include <iostream> 
 using namespace std;
 using ll = long long;
 int main()
 {
-ll k; cin>>k;
+ll k;
+cin>>k;
 int low =0; int high=100; int mid=0; int works=1;
 ll ans=0;
 while (low<high){
-mid=(low+high)/2;
-ll tp=mid;
-for (int i=0;i<9;i++){
-tp=mid*1ll*tp;
-}
-if(tp<k){
-low=mid+1;
-}else{
-high=mid; works=mid;
-ans=tp;
-}
+    mid=(low+high)/2;
+    ll tp=mid;
+    for (int i=0;i<9;i++){
+        tp=mid*1ll*tp;
+    }   
+    if(tp<k){
+        low=mid+1;
+    }else{
+        high=mid;
+        works=mid;
+        ans=tp;
+    }
 }
 int sto=-1; ll tp=ans;
 for(int i =0;i<10;i++)
@@ -32,7 +35,7 @@ string ansr="";
 for(int i=0;i<=sto;i++){
 for (int j=0;j<works-1;j++) ansr+=s[i];
 }
-for(int i=sto+1;i<10;i++) {
+for(int i=sto+1;i<10;i++){
 for(int j=0;j<works;j++) ansr+=s[i];
 }
 cout<<ansr<<"\n";
