ea21f4e89d683c5e668c2e7c52469fd125c083b7
Isaac Lee <isaacanthonylee@gmail.com>
2023-08-26 17:54:00 +0100

    Add solution for problem `D - Matrix Cascade` Link: https://codeforces.com/contest/1864/problem/D Tags: brute force, constructive algorithms, data structures, dp, greedy, math, *1700 Ref: https://codeforces.com/contest/1864/submission/220586835

--- codeforces/1864/D.cpp
+++ codeforces/1864/D.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 using ll=long long;
 const int MOD=1e9+7;
-vector<vector<int> > fen(30001, vector<int>());
 
 void solve(){
     int n;
@@ -11,25 +10,25 @@ void solve(){
     for(int i=0;i<n;i++){
         cin>>s[i];
     }
-    int dp[n+1][n+2];
-    bool opdone[n+1][n+2];
+    ll dp[n+1][n+2];
     for(int i=0;i<=n;i++){
-        for(int j=0;j<=n+1;j++){
+        for(int j=0;j<=n;j++){
                 dp[i][j]=0;
-                opdone[i][j]=0;
             }
         }
-    int op=0;
-    vector<int> diagonals(n*4);
-    vector<int> otherdiag(n*6);
+    ll op=0;
+    vector<ll> diagonals(n*4);
+    vector<ll> otherdiag(n*6);
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             dp[i][j]=dp[i-1][j];
             dp[i][j]+=diagonals[i+j];
             dp[i][j]+=otherdiag[i+n-j];
             int add=1;
-            if(s[i-1][j-1]=='0') add=0;
-            if((dp[i][j]+add)%2==1){
+            if(s[i-1][j-1]=='0') {
+                add=0;
+            }
+            if(((dp[i][j]+add)%2)==1){
                 op++;
                 diagonals[i+j]++;
                 otherdiag[i+n-j]++;
