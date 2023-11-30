d50e30a37acd53d8ccfd3c3a20638e52b361fa23
Isaac Lee <isaacanthonylee@gmail.com>
2023-11-30 16:37:00 +0000

    Add solution for problem `A - Halloumi Boxes` Link: https://codeforces.com/contest/1903/problem/A Tags: brute force, greedy, sortings, *800 Ref: https://codeforces.com/contest/1903/submission/235083316

---
+++ codeforces/1903/A.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <array>
+#include <stack>
+using namespace std;
+using ll=long long;
+const int MOD=1e9+7;
+int add(int x, int y)
+{
+    return ((x + y) % MOD + MOD) % MOD;
+}
+
+int mul(int x, int y)
+{
+    return x * 1ll * y % MOD;
+}
+
+int binpow(int x, int y)
+{
+    int z = 1;
+    while(y)
+    {
+        if(y % 2 == 1) z = mul(z, x);
+        x = mul(x, x);
+        y /= 2;
+    }
+    return z;
+}
+
+int inv(int x)
+{
+    return binpow(x, MOD - 2);
+}
+
+int divide(int x, int y) {
+    return mul(x, inv(y));
+}
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+
+    }
+    bool sorted=1;
+    for(int i=1;i<n;i++){
+        if(a[i]<a[i-1]) sorted=0;
+    }
+    if(k==1&& !sorted){
+        cout<<"NO\n";
+    }else{
+        cout<<"YES\n";
+    }
+
+
+
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+}
